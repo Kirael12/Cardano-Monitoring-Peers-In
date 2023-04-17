3bb708fff3850f92e580f92e81f36d61be061d62
Kirael12 <113426048+Kirael12@users.noreply.github.com>
2023-04-17 14:18:13 +0200

    Update peersin-bp.sh
    04/2023 Minor correction

--- peersin-bp.sh
+++ peersin-bp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-peers_in=$(ss -tnp state established 2>/dev/null | egrep "cardano-node" | egrep ":6000" | grep -v "<PRIVATE IP RELAY NODE>:6000" | awk '{gsub(/\[::ffff:/,""); gsub(/\]/,""); print $4 " --> "$3; }' | sort)
+peers_in=$(ss -tnp state established 2>/dev/null | egrep "cardano-node" | egrep "<YOUR BP IP>:<YOUR BP PORT>|<YOUR OTHER BP IP IF ANY>:<YOUR BP PORT> | awk '{print $4}' | grep -v "<YOUR BP IP>" | sort)
 peers_in_count=$(echo "$peers_in" | wc -l)
 
 printf '{\n"peers_in_count":%s}' $peers_in_count > <full path to your $HOME directory>/custom-metrics/peersin/peers_in.json
